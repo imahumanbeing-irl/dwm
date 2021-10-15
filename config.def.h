28aae95cfd3f774f5ccfa5d9192d619a6c6f58d6
imahumanbeing-irl <92551695+imahumanbeing-irl@users.noreply.github.com>
2021-10-15 22:04:51 +0000

    screenshots added
    you can now take screenshots with the print screen key

--- config.def.h
+++ config.def.h
@@ -2,20 +2,21 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 2;        /* gaps between windows */
+static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"monospace:size=7:antialias=false:autohint=false"};
+static const char *fonts[]          = {"monospace:size=7:antialias=false:autohint=false", "Pomodoro:size=8"};
 static const char dmenufont[]       = "monospace:size=7:antialias=false:autohint=false";
-static const char col_gray1[]       = "#1d1f21";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#c1c1c1";
+static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#c1c1c1";
+static const char col_border[]      = "#4a4a4a";
 static const char col_cyan[]        = "#198844";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = {col_gray3, col_gray1, col_cyan},
+	[SchemeNorm] = {col_gray3, col_gray1, col_border},
 	[SchemeSel]  = {col_gray4, col_cyan,  col_cyan},
 };
 
@@ -65,6 +66,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+        { 0,                            XK_Print,  spawn,          SHCMD("screenshot \"/home/imahumanbeing/Pictures/Screenshots/$(date +'%Y-%m-%d_%N%S').png\"") }, 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -117,4 +119,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
+
+
 
